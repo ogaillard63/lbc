ca02d8e072074deff704ef188bd5966fb2178efe
Olivier Gaillard <Olivier Gaillard>
2017-03-30 23:32:52 +0200

    correction bugs

--- dump.php
+++ dump.php
@@ -1,7 +1,13 @@
 <?php
+/**
+* @project		Dinero
+* @author		Olivier Gaillard
+* @version		1.0 du 11/12/2014
+* @desc			Dump de la base de données
+*/
 
-$path =  realpath(dirname (__FILE__)).'/inc/properties';
-$properties_filepath = $path.'/properties.ini';
+$properties_filepath = realpath(dirname (__FILE__)).'/inc/properties/properties.ini';
+if (!is_file($properties_filepath)) die("Unbale to find :  ".$properties_filepath);
 $prop = parse_ini_file($properties_filepath);
 
 backup_tables($prop['db_hostname'],$prop['db_username'],$prop['db_password'],$prop['db_name'], '*');
@@ -9,15 +15,14 @@ backup_tables($prop['db_hostname'],$prop['db_username'],$prop['db_password'],$pr
 /* backup the db OR just a table */
 function backup_tables($host, $user, $pass, $name, $tables = '*') {
 	
-	$link = mysql_connect($host,$user,$pass);
-	mysql_select_db($name,$link);
+	$link = mysqli_connect($host,$user,$pass, $name);
 	$return = '';
 
 	//get all of the tables
 	if($tables == '*') {
 		$tables = array();
-		$result = mysql_query('SHOW TABLES');
-		while($row = mysql_fetch_row($result)) {
+		$result = mysqli_query($link, 'SHOW TABLES');
+		while($row = mysqli_fetch_row($result)) {
 			$tables[] = $row[0];
 		}
 	}
@@ -28,22 +33,22 @@ function backup_tables($host, $user, $pass, $name, $tables = '*') {
 	
 	//cycle through
 	foreach($tables as $table) {
-		$result = mysql_query('SELECT * FROM '.$table);
-		$num_fields = mysql_num_fields($result);
+		$result = mysqli_query($link, 'SELECT * FROM '.$table);
+		$num_fields = mysqli_num_fields($result);
 		
 		$return.= 'DROP TABLE IF EXISTS '.$table.';';
-		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$row2 = mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE '.$table));
 		$return.= "\n\n".$row2[1].";\n\n";
 		
 		for ($i = 0; $i < $num_fields; $i++) 
 		{
-			while($row = mysql_fetch_row($result))
+			while($row = mysqli_fetch_row($result))
 			{
 				$return.= 'INSERT INTO '.$table.' VALUES(';
 				for($j=0; $j < $num_fields; $j++) 
 				{
 					$row[$j] = addslashes($row[$j]);
-					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+					$row[$j] = str_replace("\n","\\n",$row[$j]);
 					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
 					if ($j < ($num_fields-1)) { $return.= ','; }
 				}
@@ -52,7 +57,6 @@ function backup_tables($host, $user, $pass, $name, $tables = '*') {
 		}
 		$return.="\n\n\n";
 	}
-	
 	//save file
 	$filename = 'dumps/db-backup-'.time().'.sql';
     $handle = fopen($filename,'w+');
--- install/index.php
+++ install/index.php
@@ -1,8 +1,13 @@
 <?php
 
 $host="localhost"; 
+
 $root="root"; 
 $root_password=""; 
+
+$user = 'lbc';
+$pass = 'lbc';
+$db   = 'lbc'; 
 
 $req = file_get_contents ("./lbc.sql");
 $req = str_replace("\n","",$req);
@@ -10,9 +15,17 @@ $req = str_replace("\r","",$req);
 
     try {
         $dbh = new PDO("mysql:host=$host", $root, $root_password);
-        $dbh->exec($req) or die(print_r($dbh->errorInfo(), true));
-        echo "Installation de la base de données terminée !";
+        $dbh->exec("CREATE DATABASE `$db`;
+                CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+                GRANT ALL ON `$db`.* TO '$user'@'localhost';
+                FLUSH PRIVILEGES;") 
+        or die(print_r($dbh->errorInfo(), true));
+
     } catch (PDOException $e) {
         die("DB ERROR: ". $e->getMessage());
     }
+
+    // Bug encodage des données
+    $dbh->exec($req) or die(print_r($dbh->errorInfo(), true));
+    echo "Installation de la base '".$db."' terminée !";
 ?>
